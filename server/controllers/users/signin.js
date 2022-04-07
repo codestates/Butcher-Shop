f072dd7cf5afec6ec55bbd91274c73cbfc5a3466
lee chun woo <chunwoolee.work@gmail.com>
2022-04-07 14:47:29 +0900

    [FIX] signin.js 인덴트 정리

--- server/controllers/users/signin.js
+++ server/controllers/users/signin.js
@@ -1,13 +1,18 @@
 const { user } = require('../../models');
 const { generateAccessToken, sendAccessToken } = require('../tokenFunctions');
 
-module.exports = async(req, res) => {
-  const userInfo = await user.findOne({where: {userId:req.body.userId , password:req.body.password},
-  });
+module.exports = async (req, res) => {
+  const userInfo = await user.findOne(
+    {where: {userId:req.body.userId , password:req.body.password}}
+  );
   if(!userInfo) {
-    res.status(404).send('invalid user')
-  } else {
-    await generateAccessToken(userInfo.dataValues).then((data)=>
-     { res.cookie("jwt",data).status(200).send({message:'ok'})})
+    return res.status(404).send('invalid user')
   }
+  await generateAccessToken(userInfo.dataValues)
+    .then((data)=>{
+      res
+        .cookie("jwt",data)
+        .status(200)
+        .send({message:'ok'})
+    })
 };
