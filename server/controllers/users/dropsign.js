8e992c984f98756941895391a3088daf6aba8247
Mizulatte <rainbow5284@gmail.com>
2022-04-08 10:57:19 +0900

    [FIX] 회원탈퇴시 회원정보 입력 추가

--- server/controllers/users/dropsign.js
+++ server/controllers/users/dropsign.js
@@ -4,8 +4,16 @@ const { isAuthorized } = require('../tokenFunctions');
 module.exports = async (req, res) => {
     if (!isAuthorized(req)) {
         res.status(405).send('invalid')
+    }
+    const userInfo = await user.findOne({where: {
+        userId: req.body.userId,
+        password: req.body.password
+    }})
+
+    if (!userInfo) {
+        res.status(406).send('incorrect id or password')
     } else {
-        await user.destroy({where: {userId: req.body.userId}})
+        await user.destroy({where: {userId: req.body.userId, password: req.body.password}})
         res.status(206).clearCookie('jwt').send('good bye')
     }
 }
