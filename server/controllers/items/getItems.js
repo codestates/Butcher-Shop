ba2a6f10563ea8c14769d5307ca6dc3ad28d0338
lee chun woo <chunwoolee.work@gmail.com>
2022-04-11 13:34:08 +0900

    [ADD] item 조회 기능 추가2

--- server/controllers/items/getItems.js
+++ server/controllers/items/getItems.js
@@ -1,5 +1,16 @@
 const { item } = require('../../models');
 
 module.exports = async (req, res) => {
-    return res.status(200).json(await item.findAll().dataValues)
+    const hash = [
+        '소고기',
+        '돼지고기',
+        '닭고기',
+        '가공품'
+    ]
+    const result = (await item.findAll())
+        .map(item=>{
+            item.type = hash[item.type]     
+            return item
+        })
+    return res.status(200).json(result)
 }
--- server/models/item.js
+++ server/models/item.js
@@ -23,7 +23,8 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: 'item'
+      modelName: 'item',
+      timestamps: false
     }
   );
   return item;
