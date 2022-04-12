89dccbf21acd2f2a4027dbae11d6071fa06a182f
Mizulatte <rainbow5284@gmail.com>
2022-04-12 17:57:34 +0900

    [FIX] 주문요청 수정

--- server/controllers/orders/orderItems.js
+++ server/controllers/orders/orderItems.js
@@ -2,12 +2,14 @@ const { order_item } = require('../../models')
 
 module.exports = async (req, res) => {
   try{
-    console.log(req.body)
-    await order_item.create(
-      {where: {name: req.body.name, price: req.body.price}}
-    ).then(res => {
-      res.status(200).send('ok')
+    await order_item.create({
+      where: {
+        name: req.body.name,
+        price: req.body.price
+      }
     })
+    return res.status(200).send('ok')
   } catch(err) {
       throw err
-  }}
+  }
+}
